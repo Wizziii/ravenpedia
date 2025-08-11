91cbeb9d8482c57d007379cccd91c4a4981804ad
Marten <gierth.marten@gmail.com>
2025-08-11 19:31:38 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,8 +18,7 @@ const config: QuartzConfig = {
     locale: "de-DE",
     baseUrl: "ravenpedia",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
-    generateSocialImages: false,
+    defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
@@ -58,7 +57,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "filesystem"],
+        priority: ["frontmatter", "git", "filesystem"],
       }),
       Plugin.SyntaxHighlighting({
         theme: {
@@ -87,7 +86,10 @@ const config: QuartzConfig = {
       }),
       Plugin.Assets(),
       Plugin.Static(),
+      Plugin.Favicon(),
       Plugin.NotFoundPage(),
+      // Comment out CustomOgImages to speed up build time
+      //Plugin.CustomOgImages(),
     ],
   },
 }
