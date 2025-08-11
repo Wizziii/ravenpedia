801ca10232e3c3ac8d909d885ce5755f4a00e995
Marten <gierth.marten@gmail.com>
2025-08-11 23:39:14 +0200

    Enhance defaultListPageLayout with new components
    Added TagList to beforeBody and Graph, TableOfContents, and Backlinks to the right section of defaultListPageLayout to improve navigation and metadata visibility on list pages.

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,12 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [
+      Component.Breadcrumbs(),
+      Component.ArticleTitle(),
+      Component.ContentMeta(),
+      Component.TagList(),
+    ],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
@@ -46,5 +51,8 @@ export const defaultListPageLayout: PageLayout = {
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
   ],
-  right: [],
+  right: [
+    Component.Graph(),
+    Component.DesktopOnly(Component.TableOfContents()),
+    Component.Backlinks(),],
 }
